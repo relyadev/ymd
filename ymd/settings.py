56372b22691a0ff4a7a296b54d7efdb03cea26cd
relyadev <relyadev@gmail.com>
2025-08-18 21:40:34 +0300

    web callback2

--- ymd/settings.py
+++ ymd/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-36t@bt73msxun2ad%x-n4z0zmxxul=npf7^by0bowxv-y7jv6+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
